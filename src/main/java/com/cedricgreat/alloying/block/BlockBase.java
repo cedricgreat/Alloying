ff1d968aa3da81f25c89376abc1c9b0a2e1af06f
cedricgreat <cedricgreat@gmail.com>
2018-12-27 14:09:21 -0400

    block has a texture

--- src/main/java/com/cedricgreat/alloying/block/BlockBase.java
+++ src/main/java/com/cedricgreat/alloying/block/BlockBase.java
@@ -10,6 +10,7 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 
+
 public class BlockBase extends Block implements IHasModel
 {
     public BlockBase(String name, Material material)
